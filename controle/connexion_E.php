98876f021ae6aa91661f0819b619afc5c75f44a3
Beeshop <91916671+NathanJ-B-A@users.noreply.github.com>
2021-11-03 14:44:36 +0100

    Controle de connexion
    connexion_L.php, connexion_E.php

---
+++ controle/connexion_E.php
@@ -0,0 +1,32 @@
+<?php
+	session_start();
+
+	$ident=  isset($_POST['ident'])?($_POST['ident']):'';
+	$mdp=  isset($_POST['mdp'])?($_POST['mdp']):'';
+	$msg='';
+
+	if (count($_POST)==0)
+    require ("connexion_E.tpl") ;
+  else {
+	   if (!verif_ident_bd ($ident, $mdp, $resultat)) {
+	      $_SESSION['profil']= array();
+		    $msg ="erreur de saisie";
+	      require ("connexion_E.tpl") ;
+		}
+	  else {
+	     $_SESSION['profil']= $resultat[0];
+			 $url = "accueil.php";
+			 header ("Location:" .$url) ;
+
+		  }
+    }
+
+    function verif_ident_bd ($ident, $mdp, &$resultat=array()) {
+  	require ("bd_entreprise.php");
+    $resultat = estPresent($ident, $mdp);
+  	if(count($resultat)!= 0)
+      return true;
+    return false;
+  }
+
+?>
